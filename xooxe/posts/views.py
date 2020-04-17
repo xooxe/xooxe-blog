3b7cbd740c8f373aef947bccc665e81e34d90df9
Abolfazl Nazerpanah <iamxooxe@outlook.com>
2020-04-17 19:08:07 +0430

    part posts

---
+++ xooxe/posts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def posts(request):
+    return render(request , 'posts/posts.html')
--- xooxe/xooxe/urls.py
+++ xooxe/xooxe/urls.py
@@ -3,5 +3,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('' , include('pages.urls')),
+    path('posts/' , include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
